d62e9552ccd2359d7c35510ee3e0052a9eefbde5
ericlent <ericquaresma@hotmail.com>
2024-06-25 17:59:32 -0300

    Botão voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
-import { Container, Logo } from "./styles";
+import { Container, Logo, BackButton, BackIcon } from "./styles";
 import logImg from "@assets/logo.png";
 
-export function Header(){
-    return(
+type Props = {
+    showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props) {
+    return (
         <Container>
+            {
+                showBackButton &&
+                <BackButton>
+                    <BackIcon />
+                </BackButton>
+            }
             <Logo source={logImg}/>
         </Container>
     )
 }
+
