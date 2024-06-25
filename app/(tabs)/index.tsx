92888b46da32ef9d3ead0144c2615c911db90af4
ericlent <ericquaresma@hotmail.com>
2024-06-25 16:01:19 -0300

    Tela incial com imagem

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
 //import { Groups } from "@/src/screens/Groups";
+import { StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native";
 import theme from "@/src/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
+import { Header } from "@/src/components/Header";
 import { Container } from "./styles";
 //import * as S from "./styles"
 
 export function Groups() {
     return (
         <Container>
-
+            <Header />
         </Container>
     )
 }
